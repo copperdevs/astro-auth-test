d431a6e8fffc522d2ad52a26dc509bfb182b62dc
copper <79734708+copperdevs@users.noreply.github.com>
2024-08-06 20:03:53 -0500

    compat

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 import clerk from "@clerk/astro";
@@ -6,6 +6,11 @@ import clerk from "@clerk/astro";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+      configPath: "wrangler.toml",
+    },
+  }),
   integrations: [clerk()],
 });
