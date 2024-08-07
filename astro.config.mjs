8ec5c948339c7c880d8d55454e5dc73d487bfd6e
copper <79734708+copperdevs@users.noreply.github.com>
2024-08-06 19:36:35 -0500

    wait i figured it out

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from "@astrojs/cloudflare";
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [clerk()],
 });
